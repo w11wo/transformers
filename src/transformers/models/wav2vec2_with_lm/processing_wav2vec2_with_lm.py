e6142ddc74a4343fe47d7a8a1eb7de8477ecb5bd
Davidsamuel101 <davidsamuel.7878@gmail.com>
2023-07-06 07:09:01 +0000

    Fixed Alphabet Tokens with Target Language

--- src/transformers/models/wav2vec2_with_lm/processing_wav2vec2_with_lm.py
+++ src/transformers/models/wav2vec2_with_lm/processing_wav2vec2_with_lm.py
@@ -195,7 +195,10 @@ class Wav2Vec2ProcessorWithLM(ProcessorMixin):
         # we need to make sure that all of the tokenizer's except the special tokens
         # are present in the decoder's alphabet. Retrieve missing alphabet token
         # from decoder
-        tokenizer_vocab_list = list(tokenizer.get_vocab().keys())
+        if tokenizer.target_lang is not None:
+            tokenizer_vocab_list = list(tokenizer.get_vocab()[tokenizer.target_lang].keys())
+        else:
+            tokenizer_vocab_list = list(tokenizer.get_vocab().keys())
 
         # replace special tokens
         for i, token in enumerate(tokenizer_vocab_list):
